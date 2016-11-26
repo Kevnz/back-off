50fd1a78eefea9db84910baf784188e50a0dbcd6
Kevin Isom <kevinisom@Shenandoah.fritz.box>
2016-11-26 22:16:43 +1300

    tidy code

--- cb.js
+++ cb.js
@@ -1,36 +1,36 @@
 
 export default class CircuitBreaker {
-	constructor (props) {
-        props = props || {};
-        this.timesToTry = props.times || 3;
-        this.delay = props.delay || 0;
-        this.backoff = props.backoff || false;
-        super(props);
+  constructor (props) {
+    props = props || {};
+    this.timesToTry = props.times || 3;
+    this.delay = props.delay || 0;
+    this.backoff = props.backoff || false;
+    super(props);
+  }
+  tryCall (callback, timesRemaining) {
+    timesRemaining--;
+    if(timesRemaining === 0) {
+      callback({status:'Failed'});
+      return;
     }
-    tryCall (callback, timesRemaining) {
-        timesRemaining--; 
-    	if(timesRemaining === 0) {
-    		callback({status:'Failed'});
-    		return;
-    	} 
 
-    	try {
-    		callback();
-    	} catch (e) {
-    		if (this.delay > 0) {
-                console.log((this.timesToTry - timesRemaining));
-                let delay = this.backoff ? this.delay * (this.timesToTry - timesRemaining) : this.delay;
-                console.log('the delay ' + delay);
-                setTimeout(() => {
-                    this.tryCall(callback,timesRemaining);
-                }, this.delay);
-            } else {
-                this.tryCall(callback,timesRemaining);
-            }
+    try {
+      callback();
+    } catch (e) {
+      if (this.delay > 0) {
+        console.log((this.timesToTry - timesRemaining));
+        let delay = this.backoff ? this.delay * (this.timesToTry - timesRemaining) : this.delay;
+        console.log('the delay ' + delay);
+        setTimeout(() => {
+          this.tryCall(callback,timesRemaining);
+        }, this.delay);
+      } else {
+        this.tryCall(callback,timesRemaining);
+      }
 
-    	}
     }
-    execute (callback) {
-		this.tryCall(callback, this.timesToTry);
-    }
+  }
+  execute (callback) {
+    this.tryCall(callback, this.timesToTry);
+  }
 }
--- test.js
+++ test.js
@@ -2,93 +2,93 @@ import assert from 'assert';
 import CircuitBreaker from './cb';
 
 describe('The Circuit Breaker Module', () => {
-	describe('the api', () => {
-		it('should accept a function', (done) => {
+  describe('the api', () => {
+    it('should accept a function', (done) => {
 
-			let cb = new CircuitBreaker();
-			cb.execute(() => {
-				assert.ok(true, 'this got executed');	
-				done();
-			});
-		});
-		it('should execute a function a default of 3 times', (done) => {
+      let cb = new CircuitBreaker();
+      cb.execute(() => {
+        assert.ok(true, 'this got executed');
+        done();
+      });
+    });
+    it('should execute a function a default of 3 times', (done) => {
 
-			let cb = new CircuitBreaker();
-			let executeCount = 0;
-			cb.execute((finalError) => {
-				executeCount++;
-				if (executeCount === 3) {
-					assert.ok(finalError !== null, 'this got executed');
-					done();	
-				} else if (executeCount > 3) {
-					assert.fail(executeCount, 3);
-				} 
-				else {
-					throw new Error("This is an error");
-				}
-				
-			});
-		});
-		it('should allow the times to execute to be configured', (done) => {
-			let cb = new CircuitBreaker({times: 6});
-			let executeCount = 0;
-			cb.execute((finalError) => {
-				executeCount++;
-				if (executeCount === 6) {
-					assert.ok(finalError !== null, 'this got executed');
-					done();	
-				} else if (executeCount > 6) {
-					assert.fail(executeCount, 6);
-				} 
-				else {
+      let cb = new CircuitBreaker();
+      let executeCount = 0;
+      cb.execute((finalError) => {
+        executeCount++;
+        if (executeCount === 3) {
+          assert.ok(finalError !== null, 'this got executed');
+          done();
+        } else if (executeCount > 3) {
+          assert.fail(executeCount, 3);
+        }
+        else {
+          throw new Error("This is an error");
+        }
 
-					throw new Error("This is an error");
-				}
-				
-			});
-			
-		});
-		it('should be delayed between executions', (done) => {
-			let cb = new CircuitBreaker({times: 6, delay: 10});
-			let executeCount = 0;
-			let start = Date.now();
-			cb.execute((finalError) => {
-				executeCount++;
-				if (executeCount === 6) {
-					let end = Date.now();
-					assert.ok(finalError !== null, 'this got executed');
-					assert.ok(start + 50 < end && start + 70 > end, 'Should be long enough, but not to long');
-					done();	
-				} else if (executeCount > 6) {
-					assert.fail(executeCount, 6, "executed too many times");
-				} 
-				else {
-					throw new Error("This is an error");
-				}
-				
-			});
-		});
-		it('should have the delay between execution double when passed a backoff property', (done) => {
-			let cb = new CircuitBreaker({ delay: 100, backoff:true, times: 4});
-			let executeCount = 0;
-			let start = Date.now();
-			cb.execute((finalError) => {
-				executeCount++;
-				if (executeCount === 4) {
-					let end = Date.now();
-					assert.ok(finalError !== null, 'this got executed');
-					console.log(end - start);
-					assert.ok(start < end && (start + 320 > end && start + 250 < end ) , 'Should be long enough, but not to long');
-					done();	
-				} else if (executeCount > 4) {
-					assert.fail(executeCount,finalError, "executed too many times");
-				} 
-				else {
-					throw new Error("This is an error");
-				}
-				
-			});
-		});
-	});
+      });
+    });
+    it('should allow the times to execute to be configured', (done) => {
+      let cb = new CircuitBreaker({times: 6});
+      let executeCount = 0;
+      cb.execute((finalError) => {
+        executeCount++;
+        if (executeCount === 6) {
+          assert.ok(finalError !== null, 'this got executed');
+          done();
+        } else if (executeCount > 6) {
+          assert.fail(executeCount, 6);
+        }
+        else {
+
+          throw new Error("This is an error");
+        }
+
+      });
+
+    });
+    it('should be delayed between executions', (done) => {
+      let cb = new CircuitBreaker({times: 6, delay: 10});
+      let executeCount = 0;
+      let start = Date.now();
+      cb.execute((finalError) => {
+        executeCount++;
+        if (executeCount === 6) {
+          let end = Date.now();
+          assert.ok(finalError !== null, 'this got executed');
+          assert.ok(start + 50 < end && start + 70 > end, 'Should be long enough, but not to long');
+          done();
+        } else if (executeCount > 6) {
+          assert.fail(executeCount, 6, "executed too many times");
+        }
+        else {
+          throw new Error("This is an error");
+        }
+
+      });
+    });
+    it('should have the delay between execution double when passed a backoff property', (done) => {
+      let cb = new CircuitBreaker({ delay: 100, backoff:true, times: 4});
+      let executeCount = 0;
+      let start = Date.now();
+      cb.execute((finalError) => {
+        executeCount++;
+        if (executeCount === 4) {
+          let end = Date.now();
+          assert.ok(finalError !== null, 'this got executed');
+          console.log(end - start);
+          assert.ok(start < end && (start + 320 > end && start + 250 < end ) , 'Should be long enough, but not to long');
+          done();
+        } else if (executeCount > 4) {
+          assert.fail(executeCount,finalError, "executed too many times");
+        }
+        else {
+          throw new Error("This is an error");
+        }
+
+      });
+    });
+  });
 
 });
