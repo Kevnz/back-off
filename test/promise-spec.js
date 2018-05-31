37fc9b8ee6003af0f7a80d7534e0448f38eca960
Kevin Isom <kevin.isom@gmail.com>
2018-06-01 11:25:24 +1200

    timing

--- test/promise-spec.js
+++ test/promise-spec.js
@@ -155,7 +155,7 @@ describe('The Circuit Breaker Module As Promised', () => {
       });
     });
     it('should have the delay between execution double when passed a backoff property', (done) => {
-      const cb = new CircuitBreaker({ delay: 100, backoff:true, times: 4});
+      const cb = new CircuitBreaker({ delay: 105, backoff: true, times: 4});
       let executeCount = 0;
       const start = Date.now();
       cb.executeAsPromise((finalError) => {
